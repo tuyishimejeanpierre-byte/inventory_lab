57e1a091a541d313c11016f5cb7326f3b39d4110
tuyishimejeanpierre-byte <tuyishime.jeanpierre@student.moringaschool.com>
2026-07-05 17:11:55 +0300

    design system archtecture and endpoints

---
+++ data/mock_data.py
@@ -0,0 +1,42 @@
+mock_inventory = [
+    {
+        "id": 1,
+        "status": 1,
+        "product": {
+            "barcode": "737628064502",
+            "product_name": "Organic Almond Milk",
+            "brand": "Silk",
+            "ingredients": "Filtered water, almonds, cane sugar"
+        }
+    },
+    {
+        "id": 2,
+        "status": 1,
+        "product": {
+            "barcode": "3017620422003",
+            "product_name": "Nutella",
+            "brand": "Ferrero",
+            "ingredients": "Sugar, palm oil, hazelnuts, cocoa"
+        }
+    },
+    {
+        "id": 3,
+        "status": 1,
+        "product": {
+            "barcode": "5449000000996",
+            "product_name": "Coca-Cola Original Taste",
+            "brand": "Coca-Cola",
+            "ingredients": "Carbonated water, sugar, caramel colour"
+        }
+    },
+    {
+        "id": 4,
+        "status": 1,
+        "product": {
+            "barcode": "7622210449283",
+            "product_name": "Oreo Original",
+            "brand": "Oreo",
+            "ingredients": "Wheat flour, sugar, palm oil"
+        }
+    }
+]
