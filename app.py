86964d79f42a7012656531cb4c3c71a96a5ec428
tuyishimejeanpierre-byte <tuyishime.jeanpierre@student.moringaschool.com>
2026-07-05 17:32:33 +0300

    blue print creation

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 
+from config import Config
+from routes.inventory_routes import inventory_bp
+
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+app.register_blueprint(inventory_bp)
 
 
 @app.route("/")
 def home():
+
     return {
-        "message": "Inventory Management API is running."
+        "message": "Inventory Management API running."
     }
 
 
